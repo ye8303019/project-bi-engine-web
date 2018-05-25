e47d8a057eeabf52efd9a505db9de585ac5ee851
dyun <daiyang@patsnap.com>
2018-05-26 00:03:53 +0800

    chart page

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -3,24 +3,39 @@
       <el-container class="app-overview-container">
         <el-header class="app-overview-container-header">仪表盘</el-header>
         <el-main class="app-overview-container-main" style="overflow: hidden">
-          <chart-demo></chart-demo>
-          <chart-demo></chart-demo>
-          <chart-demo></chart-demo>
-          <chart-demo></chart-demo>
-          <chart-demo></chart-demo>
-          <chart-demo></chart-demo>
+          <chart-demo v-for="chartData in chartDatas" :chart-type="dealData(chartData).chartType" :chart-prop="dealData(chartData).data" :show-type="'overView'"></chart-demo>
         </el-main>
       </el-container>
     </div>
 </template>
 <script>
-  import ChartDemo from './ChartDemo.vue'
+  import ChartDemo from './ChartDemo.vue';
+  import axios from 'axios'
 
   export default {
     name:'Dashboard',
-
     components: {
       ChartDemo
+    },
+    created() {
+      axios.get('/bi/chart/list').then((result)=>{
+        let data = result.data.data;
+        this.chartDatas = data;
+      })
+    },
+    data() {
+      return {
+        chartDatas: []
+      }
+    },
+    methods: {
+      dealData(val) {
+        let opts = JSON.parse(val.options);
+        return {
+          data: opts,
+          chartType: opts.chartType
+        }
+      }
     }
   }
 </script>
